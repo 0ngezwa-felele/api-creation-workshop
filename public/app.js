4945268c7319823501c184e41fe899be494ae8a6
Ongezwa <ongezwaf27@gmail.com>
2022-04-06 15:39:09 +0200

    style and snackbar fixed

--- public/app.js
+++ public/app.js
@@ -66,9 +66,4 @@ const domFields = {
     .catch(err => {
       console.log(err);
     });
-	function myFunction() {
-		var x = document.getElementById("snackbar");
-	  
-		x.className = "show";
-	  		setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-	  }
+	
